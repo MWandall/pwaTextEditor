07a36528614111ccd2cc8730544bd88fc9d17b43
MWandall <144394104+MWandall@users.noreply.github.com>
2023-11-21 15:15:35 -0600

    bug

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -37,7 +37,7 @@ export const getDb = async () => {
   const request = store.get(1); 
   const result = await request;
 
-  console.log('result.value', result);
+  console.log('result.value', result?.value);
   return result?.value;
 };
 
